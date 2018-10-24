c2ce6e47fe0cf63c06de36151554b29ce79ce76e
StaceyWhitmore <staceywhitmore@gmail.com>
2018-10-24 11:39:34 -0600

    changed dir structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {render} from 'react-dom'
-import RandomMeUsers from './random-me-users/RandomMeUsers'
+
+//for use with <DataComponent/>
+import RandomMeUsers from './data-component/random-me-users/RandomMeUsers'
 //import CountryDropDown from './country-drop-down/CountryDropDown'
    /*
    render(
